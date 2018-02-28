8a32e7e32e048d0e422529e4451d42e2c9234295
4Chazz <chastitylawson1@gmail.com>
2018-02-28 14:15:15 -0600

    Made Timer pretty

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,46 +1,3 @@
-// var deadline = 'June 6 2018';
-
-// function getTimeRemaining(endtime) {
-//   var t = Date.parse(endtime) - Date.parse(new Date());
-//   var seconds = Math.floor((t / 1000) % 60);
-//   var minutes = Math.floor((t / 1000 / 60) % 60);
-//   var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-//   var days = Math.floor(t / (1000 * 60 * 60 * 24));
-//   return {
-//     'total': t,
-//     'days': days,
-//     'hours': hours,
-//     'minutes': minutes,
-//     'seconds': seconds
-//   };
-// }
-
-// function initializeClock(id, endtime) {
-//   var clock = document.getElementById(id);
-//   var daysSpan = clock.querySelector('.days');
-//   var hoursSpan = clock.querySelector('.hours');
-//   var minutesSpan = clock.querySelector('.minutes');
-//   var secondsSpan = clock.querySelector('.seconds');
-
-//   function updateClock() {
-//     var t = getTimeRemaining(endtime);
-
-//     daysSpan.innerHTML = t.days;
-//     hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-//     minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-//     secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-//     if (t.total <= 0) {
-//       clearInterval(timeinterval);
-//     }
-//   }
-
-//   updateClock();
-//   var timeinterval = setInterval(updateClock, 1000);
-// }
-
-// // var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
-// initializeClock('clockdiv', deadline);
 
 var deadline = new Date("June 6, 2018").getTime();
 
@@ -55,12 +12,12 @@ var deadline = new Date("June 6, 2018").getTime();
     document.getElementById("day").innerHTML = days ;
     document.getElementById("hour").innerHTML = hours;
     document.getElementById("minute").innerHTML = minutes;
-    document.getElementById("second").innerHTML =seconds;
+    // document.getElementById("second").innerHTML =seconds;
     if (t < 0) {
         clearInterval(x);
         document.getElementById("demo").innerHTML = "TIME UP";
         document.getElementById("day").innerHTML ='0';
         document.getElementById("hour").innerHTML ='0';
         document.getElementById("minute").innerHTML ='0' ;
-        document.getElementById("second").innerHTML = '0'; }
+        }
     }, 1000);
